d6cbd7c1eb1a0b3ab7b17c9bbde375507132b235
Dotaiib <36883373+Dotaiib@users.noreply.github.com>
2020-02-07 12:33:56 +0100

    Add files via upload

---
+++ commande et achat/js/js.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener("scroll", function () {
+    var mesElements = document.getElementsByClassName("reduire");
+    if (window.scrollY > 80) {
+     
+        for (var i = 0; i < mesElements.length; i++) {
+            mesElements[i].classList.add("scroller");
+        }
+    }
+    else {
+        
+        for (var i = 0; i < mesElements.length; i++) {
+            mesElements[i].classList.remove("scroller");
+        }
+    }
+ 
+});
+
+/* menu */
+var monMenu = document.getElementById("menu");
+var submenu = document.querySelector(".submenu");
+if (monMenu) {
+    monMenu.addEventListener("click", function () {
+        monMenu.classList.toggle('open');
+    });
+
+}
+
+
+/*facture*/
+
+
+document.addEventListener("load", imprimer);
+
+function imprimer() {
+    var printContents = document.getElementById("form1").innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = '<html><head> <link href="/css/font-awesome.css" rel="stylesheet"/><title></title></head><body>' + printContents + '</body>';
+    window.print();
+    document.body.innerHTML = originalContents;
+}
+
+
